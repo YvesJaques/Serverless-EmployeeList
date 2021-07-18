e1d4fbaf91133638e1e43f44324d89dc7dda0fc0
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-07-18 09:23:45 -0300

    feat: Prevent multiple employees with same name

--- src/modules/employees/useCases/createEmployeeProfile/createEmployeeUseCase.spec.ts
+++ src/modules/employees/useCases/createEmployeeProfile/createEmployeeUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../shared/errors/AppError';
 import { clearMocks, document } from "../../../../utils/dynamodbClient"
 import { CreateEmployeeUseCase } from "./createEmployeeUseCase";
 
@@ -20,4 +21,30 @@ it('Should be able to create an employee', async () => {
         expect(response.Items[0]).toHaveProperty("employeeName", "John Doe");
         expect(response.Items[0]).toHaveProperty("age", 30);
         expect(response.Items[0]).toHaveProperty("role", "Developer");
+});
+
+it('Should not be able to create multiple employees with the same name', async () => {
+    const createEmployeeUseCase = new CreateEmployeeUseCase();
+
+    //clear previous mocks
+    await clearMocks();
+
+    await document.put({
+        TableName: "employees",
+        Item: {
+            id: '1',
+            employeeName: 'John Doe',
+            age: 30,
+            role: 'Developer' 
+        }
+    }).promise();
+        
+    await expect( 
+        createEmployeeUseCase.execute({
+            employeeName: 'John Doe',
+            age: 25,
+            role: 'Tester' 
+        }),
+    ).rejects.toEqual(new AppError("An employee with this name already exists!"));
+    
 });
--- src/modules/employees/useCases/createEmployeeProfile/createEmployeeUseCase.ts
+++ src/modules/employees/useCases/createEmployeeProfile/createEmployeeUseCase.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../shared/errors/AppError";
 import { v4 as uuidv4 } from "uuid";
 import { document } from "../../../../utils/dynamodbClient"
 
@@ -7,6 +8,16 @@ class CreateEmployeeUseCase {
         employeeName,
         role,        
     }): Promise<void>{
+        const employeeAlreadyExists = await document.scan({
+            TableName: "employees",
+            FilterExpression: "employeeName = :employeeName",
+            ExpressionAttributeValues: {
+                ":employeeName": employeeName
+            }
+        }).promise();
+        
+        if(employeeAlreadyExists.Items[0]) throw new AppError("An employee with this name already exists!");
+
         await document
         .put({
             TableName: "employees",

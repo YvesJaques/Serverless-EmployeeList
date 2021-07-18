0776594e1025160417f6c4022a8177ba311e2b17
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-07-18 12:01:15 -0300

    refactor: View employee profile unit tests

--- src/modules/employees/useCases/viewEmployeeProfile/viewEmployeeUseCase.spec.ts
+++ src/modules/employees/useCases/viewEmployeeProfile/viewEmployeeUseCase.spec.ts
@@ -2,37 +2,37 @@ import { AppError } from "../../../../shared/errors/AppError";
 import { clearMocks, document } from "../../../../utils/dynamodbClient"
 import { ViewEmployeeUseCase } from "./viewEmployeeUseCase";
 
-it('Should be able to view an employee profile', async () => {
-        const viewEmployeeUseCase = new ViewEmployeeUseCase();
+let viewEmployeeUseCase: ViewEmployeeUseCase;
 
+describe("Create Employee", () => {
+    beforeEach(async () => {
         //clear previous mocks
         await clearMocks();
-                        
-        await document.put({
-            TableName: "employees",
-            Item: {
-                id: '1',
-                employeeName: 'John Doe',
-                age: 30,
-                role: 'Developer' 
-            }
-        }).promise();        
-        
-        const response = await viewEmployeeUseCase.execute({ id: "1"});
+        viewEmployeeUseCase = new ViewEmployeeUseCase();
+    });
 
-        expect(response).toHaveProperty("id");        
-        expect(response).toHaveProperty("employeeName", "John Doe");
-        expect(response).toHaveProperty("age", 30);
-        expect(response).toHaveProperty("role", "Developer");
-});
+    it('Should be able to view an employee profile', async () => {
+            await document.put({
+                TableName: "employees",
+                Item: {
+                    id: '1',
+                    employeeName: 'John Doe',
+                    age: 30,
+                    role: 'Developer' 
+                }
+            }).promise();        
+            
+            const response = await viewEmployeeUseCase.execute({ id: "1"});
 
-it('Should not be able to view an unexistent employee profile', async () => {
-    const viewEmployeeUseCase = new ViewEmployeeUseCase();
+            expect(response).toHaveProperty("id");        
+            expect(response).toHaveProperty("employeeName", "John Doe");
+            expect(response).toHaveProperty("age", 30);
+            expect(response).toHaveProperty("role", "Developer");
+    });
 
-    //clear previous mocks
-    await clearMocks();
-
-    await expect(
-        viewEmployeeUseCase.execute({ id: '1'})
-    ).rejects.toEqual(new AppError("Employee doesn't exist!"));
-});
+    it('Should not be able to view an unexistent employee profile', async () => {
+        await expect(
+            viewEmployeeUseCase.execute({ id: '1'})
+        ).rejects.toEqual(new AppError("Employee doesn't exist!"));
+    });
+})

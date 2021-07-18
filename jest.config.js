8e6d9d3d901415ad31a92e9c2713d5822d0f30fb
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-07-18 10:40:51 -0300

    Added tests coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
     "preset": "@shelf/jest-dynamodb",
     testMatch: ["**/*.spec.ts"],
     clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+    coverageDirectory: "coverage",    
+    coverageProvider: "v8",
+    coverageReporters: ["text-summary", "lcov"],
     preset: "ts-jest",
     testEnvironment: "node",
 };
